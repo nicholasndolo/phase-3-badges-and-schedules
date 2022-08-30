d16717043bd259792b5efb641bba59add42171b3
Nicholas <nicholas.nmalinda@gmail.com>
2022-08-30 22:31:14 +0300

    Lab Completed

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    array.map do |name|
+        "Hello, my name is #{name}."
+    end
+
+end
+
+def assign_rooms(array)
+    array.each_with_index.map do |name, index|
+        "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+    end
+end
+
+def printer(array)
+    batch_badge_creator(array).each do |message|
+        puts message
+    end
+    assign_rooms(array).each do |message|
+        puts message
+    end
+end
